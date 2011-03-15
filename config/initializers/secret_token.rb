bc1310907f2e0cd5366a63f406efacf06eb88d67
Joana <joana.welti.10@aberdeen.ac.uk>
2011-03-15 12:02:01 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ArtefactsService::Application.config.secret_token = 'eb16093c6e3dcc08d0128ce223c22597b942944d0650fcb1d45a268364345c3a4e58dda4cbf23b7e2aef4b63a9e95c0bc48d0dcdfdda29daaecfa0388be8e170'
