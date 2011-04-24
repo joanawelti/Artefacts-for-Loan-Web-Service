611f5470a8872bd91df4b21f769f5b354eee3d26
Joana <joana.welti.10@aberdeen.ac.uk>
2011-04-24 19:22:51 +0100

    cleaning up loan functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 ArtefactsService::Application.routes.draw do
   
+  get "pages/home"
+
+  get "pages/contact"
+
+  get "pages/about"
+
   resources :artefacts
   resources :users
   resources :users do
@@ -66,7 +72,7 @@ ArtefactsService::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => "welcome#index"
-  root :to => "sessions#new"
+  root :to => "pages#home"
 
   # See how all your routes lay out with "rake routes"
 
